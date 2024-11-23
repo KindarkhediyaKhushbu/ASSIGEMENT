93c32ca3c746755594b08be74e6cc3cd4e3e6f55
khushbu kindarkhediya <khushbukindarkhediya@gmail.com>
2024-11-23 12:54:47 +0530

    Done

---
+++ C++/pro7.cpp
@@ -0,0 +1,3 @@
+// Write a C++ program to create a class called Rectangle that has private member variables for length and width. 
+// Implement member functions to calculate the rectangle's area and perimeter.
+
